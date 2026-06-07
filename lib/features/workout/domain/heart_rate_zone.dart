be137004414dcdd566726420776ce943182ca0e5
TomBumSuChoi <qjatn0545@naver.com>
2026-06-07 17:49:22 +0900

    feat: 심박존 타입 판별 기능 추가
    - 심박수를 기반으로 심박존 타입(zone1~zone5)을 반환하는 기능 추가
    - 심박존 구간 상한값을 다음 구간 하한값보다 1 낮게 조정하여 경계 명확화
    - 심박존 범위에 심박수가 포함되는지 확인하는 `contains` 메서드 추가

--- lib/features/workout/domain/heart_rate_zone.dart
+++ lib/features/workout/domain/heart_rate_zone.dart
@@ -1,5 +1,7 @@
 import 'package:runzone/features/workout/domain/heart_rate_zone_range.dart';
 
+enum HeartRateZoneType { zone1, zone2, zone3, zone4, zone5 }
+
 final class HeartRateZone {
   final HeartRateZoneRange zone1;
   final HeartRateZoneRange zone2;
