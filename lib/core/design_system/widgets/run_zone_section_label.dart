b1093cd48d9296172a1f2108390e11558bd6cfe0
TomBumSuChoi <qjatn0545@naver.com>
2026-06-07 22:48:17 +0900

    feat: 런존 섹션 레이블 위젯 추가

---
+++ lib/core/design_system/widgets/run_zone_section_label.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+
+import '../app_colors.dart';
+import '../app_typography.dart';
+
+final class RunZoneSectionLabel extends StatelessWidget {
+  const RunZoneSectionLabel(this.text, {super.key});
+
+  final String text;
+
+  @override
+  Widget build(BuildContext context) {
+    return Text(
+      text,
+      style: AppTypography.eyebrow.copyWith(color: AppColors.mutedText),
+    );
+  }
+}
