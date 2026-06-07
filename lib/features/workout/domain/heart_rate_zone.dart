646da2beed41b0a0df6ca3a91c949d07e94136d2
TomBumSuChoi <qjatn0545@naver.com>
2026-06-07 18:42:16 +0900

    refactor: HeartRateZone 클래스에 Equatable 상속

--- lib/features/workout/domain/heart_rate_zone.dart
+++ lib/features/workout/domain/heart_rate_zone.dart
@@ -1,19 +1,23 @@
+import 'package:equatable/equatable.dart';
 import 'package:runzone/features/workout/domain/heart_rate_zone_range.dart';
 
 enum HeartRateZoneType { zone1, zone2, zone3, zone4, zone5 }
 
-final class HeartRateZone {
+final class HeartRateZone extends Equatable {
   final HeartRateZoneRange zone1;
   final HeartRateZoneRange zone2;
   final HeartRateZoneRange zone3;
   final HeartRateZoneRange zone4;
   final HeartRateZoneRange zone5;
 
-  HeartRateZone({
+  const HeartRateZone({
     required this.zone1,
     required this.zone2,
     required this.zone3,
     required this.zone4,
     required this.zone5,
   });
+
+  @override
+  List<Object?> get props => [zone1, zone2, zone3, zone4, zone5];
 }
