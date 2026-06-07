d1c062c69926e81083216029751c28cea1c571fd
TomBumSuChoi <qjatn0545@naver.com>
2026-06-07 18:40:06 +0900

    refactor: HeartRateZoneRange equatable 구현
    - HeartRateZoneRange 객체 간 값 기반 동등성 비교를 지원하도록 변경
    - HeartRateZoneRange 생성자를 const로 지정
    - equatable 패키지 import 추가

--- lib/features/workout/domain/heart_rate_zone_range.dart
+++ lib/features/workout/domain/heart_rate_zone_range.dart
@@ -1,10 +1,15 @@
-final class HeartRateZoneRange {
+import 'package:equatable/equatable.dart';
+
+final class HeartRateZoneRange extends Equatable {
   final int lower;
   final int upper;
 
-  HeartRateZoneRange({required this.lower, required this.upper});
+  const HeartRateZoneRange({required this.lower, required this.upper});
 
   bool contains(int heartRate) {
     return heartRate >= lower && heartRate <= upper;
   }
+
+  @override
+  List<Object?> get props => [lower, upper];
 }
