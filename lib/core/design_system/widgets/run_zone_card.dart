f9a8262c69a67e2708b2311b5287a45ceeb56f4c
TomBumSuChoi <qjatn0545@naver.com>
2026-06-10 21:34:29 +0900

    feat: 디자인 시스템 색상 및 간격 추가

--- lib/core/design_system/widgets/run_zone_card.dart
+++ lib/core/design_system/widgets/run_zone_card.dart
@@ -1,25 +1,22 @@
 import 'package:flutter/material.dart';
 
-import '../app_colors.dart';
-import '../app_spacing.dart';
 import '../app_radius.dart';
+import '../app_semantic_colors.dart';
+import '../app_spacing.dart';
 
 final class RunZoneCard extends StatelessWidget {
-  const RunZoneCard({
-    required this.child,
-    this.padding = AppSpacing.cardInsets,
-    super.key,
-  });
+  const RunZoneCard({required this.child, this.padding = AppSpacing.cardInsets, super.key});
 
   final Widget child;
   final EdgeInsetsGeometry padding;
 
   @override
   Widget build(BuildContext context) {
+    final colors = AppSemanticColors.of(context);
     return DecoratedBox(
       decoration: BoxDecoration(
-        color: AppColors.surface,
-        border: Border.all(color: AppColors.line),
+        color: colors.surface,
+        border: Border.all(color: colors.line),
         borderRadius: AppRadius.mediumBorder,
       ),
       child: Padding(padding: padding, child: child),
