3e75c23c12c255ff6cce7efff9ec9dbe7bc69c36
TomBumSuChoi <qjatn0545@naver.com>
2026-06-10 10:10:39 +0900

    feat: 심박존 설정을 위한 상태 관리 로직 추가
    - 각 필드 변경에 대한 액션을 cubit에 추가

--- lib/features/workout/presentation/cubit/heart_rate_zone_setup_cubit.dart
+++ lib/features/workout/presentation/cubit/heart_rate_zone_setup_cubit.dart
@@ -1,28 +1,41 @@
 import 'package:bloc/bloc.dart';
 import 'package:equatable/equatable.dart';
+import 'package:runzone/features/workout/domain/gender.dart';
 import 'package:runzone/features/workout/domain/heart_rate_zone.dart';
 import 'package:runzone/features/workout/domain/heart_rate_zone_calculator.dart';
+import 'package:runzone/features/workout/domain/height.dart';
+import 'package:runzone/features/workout/domain/running_career.dart';
+import 'package:runzone/features/workout/domain/weekly_frequency.dart';
+import 'package:runzone/features/workout/domain/weight.dart';
 
 part 'heart_rate_zone_setup_state.dart';
 
 const HeartRateZoneCalculator _calculator = HeartRateZoneCalculator();
 
 final class HeartRateZoneSetupCubit extends Cubit<HeartRateZoneSetupState> {
-  HeartRateZoneSetupCubit() : super(const HeartRateZoneSetupState(age: _defaultAge));
-
-  static const int _defaultAge = 30;
+  HeartRateZoneSetupCubit() : super(HeartRateZoneSetupState.initial());
 
   void incrementAge() {
     final int next = state.age + 1;
     if (_calculator.isValidAge(next)) {
-      emit(HeartRateZoneSetupState(age: next));
+      emit(state.copyWith(age: next));
     }
   }
 
   void decrementAge() {
     final int previous = state.age - 1;
     if (_calculator.isValidAge(previous)) {
-      emit(HeartRateZoneSetupState(age: previous));
+      emit(state.copyWith(age: previous));
     }
   }
+
+  void changeGender(Gender gender) => emit(state.copyWith(gender: gender));
+
+  void changeHeight(int centimeters) => emit(state.copyWith(height: Height(centimeters)));
+
+  void changeWeight(int kilograms) => emit(state.copyWith(weight: Weight(kilograms)));
+
+  void changeCareer(RunningCareer career) => emit(state.copyWith(career: career));
+
+  void changeWeeklyFrequency(int count) => emit(state.copyWith(weeklyFrequency: WeeklyFrequency(count)));
 }
--- lib/features/workout/presentation/cubit/heart_rate_zone_setup_state.dart
+++ lib/features/workout/presentation/cubit/heart_rate_zone_setup_state.dart
@@ -1,12 +1,51 @@
 part of 'heart_rate_zone_setup_cubit.dart';
 
 final class HeartRateZoneSetupState extends Equatable {
-  const HeartRateZoneSetupState({required this.age});
+  const HeartRateZoneSetupState({
+    required this.age,
+    required this.gender,
+    required this.height,
+    required this.weight,
+    required this.career,
+    required this.weeklyFrequency,
+  });
+
+  factory HeartRateZoneSetupState.initial() => HeartRateZoneSetupState(
+    age: 30,
+    gender: Gender.male,
+    height: Height(170),
+    weight: Weight(65),
+    career: RunningCareer.beginner,
+    weeklyFrequency: WeeklyFrequency(3),
+  );
 
   final int age;
+  final Gender gender;
+  final Height height;
+  final Weight weight;
+  final RunningCareer career;
+  final WeeklyFrequency weeklyFrequency;
 
   HeartRateZone get zone => _calculator.getHeartRateZone(age: age);
 
+  HeartRateZoneSetupState copyWith({
+    int? age,
+    Gender? gender,
+    Height? height,
+    Weight? weight,
+    RunningCareer? career,
+    WeeklyFrequency? weeklyFrequency,
+  }) {
+    return HeartRateZoneSetupState(
+      age: age ?? this.age,
+      gender: gender ?? this.gender,
+      height: height ?? this.height,
+      weight: weight ?? this.weight,
+      career: career ?? this.career,
+      weeklyFrequency: weeklyFrequency ?? this.weeklyFrequency,
+    );
+  }
+
   @override
-  List<Object?> get props => [age];
+  List<Object?> get props => [age, gender, height, weight, career, weeklyFrequency];
 }
