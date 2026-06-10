43f06507e7c570f3c95f0573b14d67272f25c334
TomBumSuChoi <qjatn0545@naver.com>
2026-06-10 17:32:55 +0900

    feat: RunZoneProgressIndicator 위젯 추가

---
+++ lib/core/design_system/widgets/run_zone_progress_indicator.dart
@@ -0,0 +1,45 @@
+import 'package:flutter/material.dart';
+import 'package:runzone/core/design_system/app_radius.dart';
+import 'package:runzone/core/design_system/app_semantic_colors.dart';
+import 'package:runzone/core/design_system/app_sizing.dart';
+import 'package:runzone/core/design_system/app_spacing.dart';
+
+final class RunZoneProgressIndicator extends StatelessWidget {
+  const RunZoneProgressIndicator({required this.totalSteps, required this.currentStep, super.key})
+    : assert(totalSteps > 0),
+      assert(currentStep >= 1 && currentStep <= totalSteps);
+
+  final int totalSteps;
+  final int currentStep;
+
+  @override
+  Widget build(BuildContext context) {
+    final colors = AppSemanticColors.of(context);
+    final segments = <Widget>[];
+
+    for (var i = 0; i < totalSteps; i++) {
+      final color = i < currentStep ? colors.progressFill : colors.progressTrack;
+      segments.add(_Segment(color: color));
+    }
+
+    return Row(spacing: AppSpacing.tight, children: segments);
+  }
+}
+
+final class _Segment extends StatelessWidget {
+  const _Segment({required this.color});
+
+  final Color color;
+
+  @override
+  Widget build(BuildContext context) {
+    return Expanded(
+      child: SizedBox(
+        height: AppSizing.progressSegmentHeight,
+        child: DecoratedBox(
+          decoration: BoxDecoration(color: color, borderRadius: AppRadius.progressBorder),
+        ),
+      ),
+    );
+  }
+}
