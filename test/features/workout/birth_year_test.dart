b290590e35bd9573198cd5d6c9f0e3c6c3c420b6
TomBumSuChoi <qjatn0545@naver.com>
2026-06-11 10:18:15 +0900

    feat: 출생연도 도메인 객체 추가
    - BirthYear 클래스를 추가하여 1940년부터 2012년까지의 유효한 출생연도를 나타냅니다.
    - 연도 증가 및 감소 기능을 제공하며, 유효 범위 밖의 값에 대한 오류 처리를 포함합니다.
    - 관련 테스트 코드를 작성하여 기능의 정확성을 검증합니다.

---
+++ test/features/workout/birth_year_test.dart
@@ -0,0 +1,35 @@
+import 'package:flutter_test/flutter_test.dart';
+import 'package:runzone/features/workout/domain/birth_year.dart';
+
+void main() {
+  test('Given 경계를 포함한 유효한 출생연도 When 생성하면 Then 해당 값을 가진다', () {
+    // 유효 범위 경계: 1940~2012
+    for (final int year in [1940, 2012]) {
+      expect(BirthYear(year).year, year, reason: '$year');
+    }
+  });
+
+  test('Given 경계를 벗어난 출생연도 When 생성하면 Then 입력값 오류가 발생한다', () {
+    for (final int year in [1939, 2013]) {
+      BirthYear create() => BirthYear(year);
+
+      expect(create, throwsArgumentError, reason: '$year');
+    }
+  });
+
+  test('Given 출생연도 When incremented 하면 Then 1 증가한 값을 반환한다', () {
+    expect(BirthYear(1996).incremented(), BirthYear(1997));
+  });
+
+  test('Given 최대 출생연도 When incremented 하면 Then 그대로 유지된다', () {
+    expect(BirthYear(2012).incremented(), BirthYear(2012));
+  });
+
+  test('Given 출생연도 When decremented 하면 Then 1 감소한 값을 반환한다', () {
+    expect(BirthYear(1996).decremented(), BirthYear(1995));
+  });
+
+  test('Given 최소 출생연도 When decremented 하면 Then 그대로 유지된다', () {
+    expect(BirthYear(1940).decremented(), BirthYear(1940));
+  });
+}
