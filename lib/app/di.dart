96e32912a383578f99481c709ecb47069446e554
TomBumSuChoi <qjatn0545@naver.com>
2026-06-11 13:15:41 +0900

    feat: 운동 프로필 설정 화면 구현
    - Shared Preferences를 이용한 ProfileRepository 구현 및 GetIt DI 설정
    - HeartRateZoneSetupScreen의 UI 컴포넌트 및 로직 분리
    - 성별, 출생 연도, 키, 체중, 러닝 경력, 주간 러닝 빈도 등 프로필 정보 입력 기능 추가

---
+++ lib/app/di.dart
@@ -0,0 +1,20 @@
+import 'package:get_it/get_it.dart';
+import 'package:runzone/features/workout/data/shared_preferences_profile_repository.dart';
+import 'package:runzone/features/workout/domain/profile_repository.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+
+final GetIt getIt = GetIt.instance;
+
+Future<void> configureDependencies() async {
+  final SharedPreferences preferences = await SharedPreferences.getInstance();
+
+  if (!getIt.isRegistered<SharedPreferences>()) {
+    getIt.registerSingleton<SharedPreferences>(preferences);
+  }
+
+  if (!getIt.isRegistered<ProfileRepository>()) {
+    getIt.registerLazySingleton<ProfileRepository>(
+      () => SharedPreferencesProfileRepository(getIt<SharedPreferences>()),
+    );
+  }
+}
