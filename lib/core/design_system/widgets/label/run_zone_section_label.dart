ec930d6c29c100e2cec4d39029213283ef556903
TomBumSuChoi <qjatn0545@naver.com>
2026-06-14 19:43:19 +0900

    feat: 텍스트 위젯에 color 속성 추가

--- lib/core/design_system/widgets/label/run_zone_section_label.dart
+++ lib/core/design_system/widgets/label/run_zone_section_label.dart
@@ -1,15 +1,17 @@
 import 'package:flutter/material.dart';
 
-import '../../app_colors.dart';
 import '../../app_typography.dart';
 
 final class RunZoneSectionLabel extends StatelessWidget {
-  const RunZoneSectionLabel(this.text, {super.key});
+  const RunZoneSectionLabel(this.text, {this.color, super.key});
 
   final String text;
+  final Color? color;
 
   @override
   Widget build(BuildContext context) {
-    return Text(text, style: AppTypography.eyebrow.copyWith(color: AppColors.gray500));
+    final colorScheme = Theme.of(context).colorScheme;
+
+    return Text(text, style: AppTypography.eyebrow.copyWith(color: color ?? colorScheme.onSurfaceVariant));
   }
 }
--- lib/core/design_system/widgets/text/description_label.dart
+++ lib/core/design_system/widgets/text/description_label.dart
@@ -1,15 +1,16 @@
 import 'package:flutter/material.dart';
 
 final class RunZoneDescriptionLabel extends StatelessWidget {
-  const RunZoneDescriptionLabel(this.text, {super.key});
+  const RunZoneDescriptionLabel(this.text, {this.color, super.key});
 
   final String text;
+  final Color? color;
 
   @override
   Widget build(BuildContext context) {
     final colorScheme = Theme.of(context).colorScheme;
     final textTheme = Theme.of(context).textTheme;
 
-    return Text(text, style: textTheme.bodyMedium?.copyWith(color: colorScheme.onSurfaceVariant));
+    return Text(text, style: textTheme.bodyMedium?.copyWith(color: color ?? colorScheme.onSurfaceVariant));
   }
 }
