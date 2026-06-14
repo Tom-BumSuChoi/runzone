7b68d2c83e97fb0d2681204c191bc1103ae6ed99
TomBumSuChoi <qjatn0545@naver.com>
2026-06-14 19:41:54 +0900

    feat: RunZoneCaptionLabel에 color 속성 추가
    - Text 위젯의 style에 color 속성을 직접 전달하도록 수정

--- lib/core/design_system/widgets/text/caption_label.dart
+++ lib/core/design_system/widgets/text/caption_label.dart
@@ -1,15 +1,16 @@
 import 'package:flutter/material.dart';
 
 final class RunZoneCaptionLabel extends StatelessWidget {
-  const RunZoneCaptionLabel(this.text, {super.key});
+  const RunZoneCaptionLabel(this.text, {this.color, super.key});
 
   final String text;
+  final Color? color;
 
   @override
   Widget build(BuildContext context) {
     final colorScheme = Theme.of(context).colorScheme;
     final textTheme = Theme.of(context).textTheme;
 
-    return Text(text, style: textTheme.labelSmall?.copyWith(color: colorScheme.onSurface));
+    return Text(text, style: textTheme.labelSmall?.copyWith(color: color ?? colorScheme.onSurface));
   }
 }
