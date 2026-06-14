063f6b0be3abb16fbb9355fdb576939edb5aad4a
TomBumSuChoi <qjatn0545@naver.com>
2026-06-14 19:43:19 +0900

    feat: 텍스트 위젯에 color 속성 추가

--- lib/core/design_system/widgets/text/title_label.dart
+++ lib/core/design_system/widgets/text/title_label.dart
@@ -1,12 +1,13 @@
 import 'package:flutter/material.dart';
 
 final class RunZoneTitleLabel extends StatelessWidget {
-  const RunZoneTitleLabel(this.text, {super.key});
+  const RunZoneTitleLabel(this.text, {this.color, super.key});
 
   final String text;
+  final Color? color;
 
   @override
   Widget build(BuildContext context) {
-    return Text(text, style: Theme.of(context).textTheme.headlineSmall);
+    return Text(text, style: Theme.of(context).textTheme.headlineSmall?.copyWith(color: color));
   }
 }
