af50fbe202f8d9c64a68c118f1afca747e177caa
TomBumSuChoi <qjatn0545@naver.com>
2026-06-14 20:14:40 +0900

    feat: 색상 스킴을 추가

---
+++ lib/core/design_system/app_color_scheme.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+import 'app_colors.dart';
+
+extension RunZoneColorScheme on ColorScheme {
+  Color get zoneOne => AppColors.blue500;
+  Color get zoneTwo => AppColors.green500;
+  Color get zoneThree => AppColors.yellow500;
+  Color get zoneFour => AppColors.orange500;
+  Color get zoneFive => AppColors.red500;
+}
