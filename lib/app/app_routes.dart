9043133592b60db4bc72a65aa7c593b6ada4e876
TomBumSuChoi <qjatn0545@naver.com>
2026-06-15 21:58:44 +0900

    feat: 앱 라우팅을 위한 AppRoutes 클래스 추가

---
+++ lib/app/app_routes.dart
@@ -0,0 +1,7 @@
+abstract final class AppRoutes {
+  static const root = '/';
+  static const splash = '/splash';
+  static const profileSetup = '/profile/setup';
+  static const home = '/home';
+  static const workoutSetup = '/workout/setup';
+}
