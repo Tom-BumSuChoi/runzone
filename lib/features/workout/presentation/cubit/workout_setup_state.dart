89222490714c6b0aa4b15b8f7c5640297786863b
TomBumSuChoi <qjatn0545@naver.com>
2026-06-16 14:33:23 +0900

    feat: 러닝머신 연결 상태 토글 기능 추가
    - WorkoutDeviceStatusCard에서 러닝머신 연결 상태를 표시하고 토글할 수 있도록 UI를 업데이트했습니다.
    - 해당 변경 사항에 대한 단위 테스트를 추가했습니다.

--- lib/features/workout/presentation/cubit/workout_setup_state.dart
+++ lib/features/workout/presentation/cubit/workout_setup_state.dart
@@ -5,7 +5,7 @@ enum WorkoutEnvironment { indoor, outdoor }
 enum WorkoutTrainingType { zoneTwo, interval, free }
 
 final class WorkoutSetupState extends Equatable {
-  const WorkoutSetupState({required this.environment, required this.trainingType});
+  const WorkoutSetupState({required this.environment, required this.trainingType, this.isTreadmillConnected = false});
 
   factory WorkoutSetupState.initial() {
     return const WorkoutSetupState(environment: WorkoutEnvironment.indoor, trainingType: WorkoutTrainingType.zoneTwo);
@@ -13,14 +13,20 @@ final class WorkoutSetupState extends Equatable {
 
   final WorkoutEnvironment environment;
   final WorkoutTrainingType trainingType;
+  final bool isTreadmillConnected;
 
-  WorkoutSetupState copyWith({WorkoutEnvironment? environment, WorkoutTrainingType? trainingType}) {
+  WorkoutSetupState copyWith({
+    WorkoutEnvironment? environment,
+    WorkoutTrainingType? trainingType,
+    bool? isTreadmillConnected,
+  }) {
     return WorkoutSetupState(
       environment: environment ?? this.environment,
       trainingType: trainingType ?? this.trainingType,
+      isTreadmillConnected: isTreadmillConnected ?? this.isTreadmillConnected,
     );
   }
 
   @override
-  List<Object?> get props => [environment, trainingType];
+  List<Object?> get props => [environment, trainingType, isTreadmillConnected];
 }
