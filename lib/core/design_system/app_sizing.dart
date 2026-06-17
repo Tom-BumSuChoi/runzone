153b3c07b482e8bad57fb2df9f670800fdc67813
TomBumSuChoi <qjatn0545@naver.com>
2026-06-17 23:05:47 +0900

    feat: RunZoneBlinkingBadge 위젯 추가 및 애니메이션 구현

--- lib/core/design_system/app_sizing.dart
+++ lib/core/design_system/app_sizing.dart
@@ -1,5 +1,6 @@
 abstract final class AppSizing {
   static const colorSwatch = 8.0;
+  static const connectionIndicatorSize = 6.0;
   static const indicatorSize = 8.0;
   static const inlineIconSize = 14.0;
   static const heartRateZoneBarHeight = 100.0;
